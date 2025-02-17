f8408fdf04d60cc15388c791f86603a08a9e24a1
selajdin89 <selajdinmemeti89@gmail.com>
2025-02-17 13:02:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import NavBar from "./components/navbar/NavBar";
+
+function App() {
+	return (
+		<div>
+			<NavBar />
+			<Header />
+			<Hero />
+		</div>
+	);
+}
+
+export default App;
