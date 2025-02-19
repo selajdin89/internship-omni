ab1a2f0e88b763ab0f4d5ba860803cde8342e0f4
Flamur Avdyli <avdyliflamur23@gmail.com>
2025-02-19 16:12:49 +0100

    NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,16 @@
 import "./NavBar.scss";
+import logo from "../../assets/omni-logo.png";
 
 const NavBar = () => {
-	return <div className="navbar">NavBar</div>;
+	return <div className="navbar">
+		<img src={logo} alt="Omni logo" className="navbar-logo" />
+		<nav>
+			<a href="#">Hiring can be stressful</a>
+			<a href="#">Our Process</a>
+			<a href="#">Why Choose Us</a>
+			<a href="#">Contact Us</a>
+		</nav>
+	</div>;
 };
 
 export default NavBar;
