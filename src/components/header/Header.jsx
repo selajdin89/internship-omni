e3ac8f855d6975a9dbea54289366b384068e87b0
Flamur Avdyli <145875910+Flamuravd@users.noreply.github.com>
2025-02-26 18:05:32 +0100

    Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,17 @@
 import "./Header.scss";
+import headerimage from "../../assets/header-img.png";
 
 const Header = () => {
-	return <div className="header">Header</div>;
+	return <div className="header">
+		<img className="headerImg" src={headerimage} alt="header section image"/>
+		<h1 className="headerHeading">
+			Are You <br/> Struggling to <br/> Hire Good Talent <br/> Within Your <br/> Budget?
+		</h1>
+
+		<button className="headerBtn">
+			FIND TOP TALENT NOW
+		</button>
+	</div>;
 };
 
 export default Header;
