a274707ea8630ed6401d3dfaba2249bf8f96305a
Flamur Avdyli <145875910+Flamuravd@users.noreply.github.com>
2025-03-22 20:49:17 +0100

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,37 +2,30 @@ import "./Hero.scss";
 import heroimage from "../../assets/hero-img.jpeg";
 
 const Hero = () => {
-	return <div className="hero">
-		<img className="heroImg" src={heroimage} alt="hero section image" />
+  return (
+    <div className="hero">
+      <img className="heroImg" src={heroimage} alt="hero section image" />
 
-		<p className="heroParagraph">
-			Are you <strong>frustrated</strong> by the overwhelming challenges of <strong>recruiting work-from-
-			<br/>
-			home employees?</strong> Do budget constraints, expensive labor markets, and high 
-			<br/>
-			recruitment costs leave you <strong>feeling stuck?</strong> Are you tired of the endless struggle 
-			<br/>
-			to find qualified candidates, <strong>worried</strong> about the uncertainty of hiring offshore 
-			<br/>
-			resources, or <strong>stressed</strong> by inflexible hiring solutions and complicated 
-			<br/>
-			onboarding processes?
-		</p>
+      <div className="heroContent">
+        <p className="heroParagraph">
+          Are you <strong>frustrated</strong> by the overwhelming challenges of{" "}
+          <strong>recruiting work-from-home employees?</strong> Do budget constraints, expensive labor
+          markets, and high recruitment costs leave you <strong>feeling stuck?</strong> Are you tired
+          of the endless struggle to find qualified candidates, <strong>worried</strong> about the
+          uncertainty of hiring offshore resources, or <strong>stressed</strong> by inflexible hiring
+          solutions and complicated onboarding processes?
+        </p>
 
-		<p className="heroParagraph2">
-			- Stress no more
-		</p>
+        <p className="heroParagraph2">- Stress no more</p>
 
-		<h1 className="heroHeading">
-			We make hiring top talent
-			<br/> 
-			within your budget easy!
-		</h1>
+        <h1 className="heroHeading">
+          We make hiring top talent within your budget easy!
+        </h1>
 
-		<button className="heroBtn">
-			TALK TO ONE OUR HIRING SPECIALIST
-		</button>
-	</div>;
+        <button className="heroBtn">TALK TO ONE OUR HIRING SPECIALIST</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
